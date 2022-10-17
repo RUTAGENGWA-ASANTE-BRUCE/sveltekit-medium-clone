3b57b63f8855c0b1a412380e94c54d962f36f7fd
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-10-17 10:50:55 +0200

    fit: adding the Medium witing story functionality

---
+++ src/routes/me/settings/+layout.svelte
@@ -0,0 +1,25 @@
+<script type="ts">
+    import TabNavigation from "../../../components/TabNavigation.svelte";
+    let links=[
+        {
+            title:'Following',
+            path:'/me/following'
+        },
+        {
+            title:'Reading history',
+            path:'/me/readinghistory'
+        },
+        {
+            title:'Muted',
+            path:'/me/settings/mute'
+        },
+        {
+            title:'Suggestions',
+            path:'/me/following/suggestions'
+        },
+    ]
+</script>
+<h1 class="text-[50px] font-bold">Refine recommendations</h1>
+<p class="text-gray-500 my-5 ">Adjust recommendations by updating what you’re following, your reading history, and who you’ve muted.</p>
+<TabNavigation links={links} />
+<slot></slot>
